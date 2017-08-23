b50612243174ac875a30923891daac0a01752368
gotti <spacelyjoe@gmail.com>
2017-08-23 13:38:00 -0400

    Refactored code

--- src/components/EZNavItem/EZNavItem.js
+++ src/components/EZNavItem/EZNavItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+require('./EZNavItem.sass');
 
 /** Nav Item for Nav list */
 
-function EZNavItem({value}){
+function EZNavItem({ value, isMobileView }){
+
+    const navItemClass = classNames({
+        'ez-nav-item': true,
+        'ez-nav-item--mobile': isMobileView
+    });
 
     return (
-        <li className = "ez-nav-item">
+        <li className={navItemClass}>
             { value }
         </li>
     )
@@ -14,11 +21,14 @@ function EZNavItem({value}){
 
 EZNavItem.propTypes = {
     /** What the nav item displays. */
-    value: PropTypes.string.isRequired
+    value: PropTypes.string.isRequired,
+
+    /** If the screen size is small enough to be showing the mobile layout. */
+    isMobileView: PropTypes.bool
 }
 
 EZNavItem.defaultProps = {
-
+    isMobileView: false
 };
 
 export default EZNavItem;
--- src/components/EZNavItem/EZNavItem.spec.js
+++ src/components/EZNavItem/EZNavItem.spec.js
@@ -15,8 +15,17 @@ test('creates a list item with the value to correctly be displayed inside of it'
     expect(wrapper.find('li').text()).toContain('Test')
 });
 
-test('has the correct classname ', () => {
+test('has the correct classname of just ez-nav-tem ', () => {
     const wrapper = shallow(<EZNavItem value={'Test'}/>)
     expect(wrapper.find('.ez-nav-item')).toHaveLength(1)
+    expect(wrapper.find('.ez-nav-item--mobile')).toHaveLength(0)
+
 });
 
+
+test('has the correct classname of ez-nav-item and ez-nav-item-mobile', () => {
+    const wrapper = shallow(<EZNavItem isMobileView={true} value={'Test'}/>)
+    expect(wrapper.find('.ez-nav-item')).toHaveLength(1)
+    expect(wrapper.find('.ez-nav-item--mobile')).toHaveLength(1)
+
+});
